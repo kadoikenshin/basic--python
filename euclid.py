345ff932e653c78b4cec5f27b94e3ba974582600
kadoi219 <kenshin.219@icloud.com>
2024-08-19 10:12:58 +0900

    課題

--- euclid.py
+++ euclid.py
@@ -1,4 +1,16 @@
-a = input("a の値を入力: ")
-b = input("b の値を入力: ")
 
-# TODO
+
+def gcd(a, b):
+    while b != 0:
+        a, b = b, a % b
+        return a
+
+
+# (1)の最大公約数
+print(f"GCD of 10 and 20: {gcd(10, 20)}")
+
+# (2)の最大公約数
+print(f"GCD of 14 and 91: {gcd(14, 91)}")
+
+# (3)の最大公約数
+print(f"GCD of 91 and 14: {gcd(91, 14)}")
--- trapezoidal_integral.py
+++ trapezoidal_integral.py
@@ -3,3 +3,20 @@ from math import sin
 # print(sin(0))
 # >>> 0
 # -----------
+
+
+def trapezoidal_inteffration(f, a, b, n):
+    h = (b - a) / n
+    result = 0.5 * (f(a) + f(b))
+    for i in range(1, n):
+        result += f(a + i * h)
+        result *= h
+        return result
+
+
+a = 0
+b = math.pi / 2
+n = 100
+
+integral_value = trapezoidal_ integration(math.sin, a, b, n)
+print(f"The approximate integral of sin(x) from 0 to π/2 is: {integral_value}")
